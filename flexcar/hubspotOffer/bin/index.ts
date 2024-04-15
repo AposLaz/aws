ad7ec1cc1b8fab7cf5af05b021f7affc6b7cc7e1
Apostolos Lazidis <39645726+AposLaz@users.noreply.github.com>
2024-04-15 18:30:31 +0300

    Delete flexcar/hubspotOffer directory

--- flexcar/hubspotOffer/bin/index.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import "source-map-support/register";
-import * as cdk from "aws-cdk-lib";
-import { StagingHubspotOffer } from "../lib";
-
-const app = new cdk.App();
-new StagingHubspotOffer(app);
